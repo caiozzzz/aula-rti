be5843d5faff188da7197d0621a9c848d63cd39d
caiozzzz <160080392+caiozzzz@users.noreply.github.com>
2024-11-04 23:35:09 +0000

    CALCULOCOMPRA

--- caelum-arquivos-curso-web/mirrorfashion/js/total.js
+++ caelum-arquivos-curso-web/mirrorfashion/js/total.js
@@ -1,15 +1,15 @@
-var $input_quantidade =document.queryselector("#qtd");
-var $output_total = document.queryselctor("#total");
+var $input_quantidade =document.querySelector("#qtd");
+var $output_total = document.querySelector("#total");
 
 $input_quantidade.oninput = function () {
-    var preco = document.queryselector ("#preco").textContent;
+    var preco = document.querySelector("#preco").textContent;
     preco = preco.replace("R$" , "");
     preco = preco.replace("," , ".");
-    preco = parseFloat  (preco)
+    preco = parseFloat(preco)
 
     var quantidade = $input_quantidade.value;
     var total = quantidade * preco;
-    total = "R$" + total.toFixed(2)
+    total = "R$ " + total.toFixed(2)
     total = total.replace("." , ",")
 
     $output_total.value = total;
