6a14e3c56d43962b6544689e0e8f85f16a1226c5
caiozzzz <160080392+caiozzzz@users.noreply.github.com>
2024-11-18 23:23:11 +0000

    bottomkjkj

---
+++ caelum-arquivos-curso-web/mirrorfashion/js/banner.js
@@ -0,0 +1,22 @@
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png" ];
+var bannerAtual=0;
+
+function trocabanner () {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector ('.banner-destaque img').src = banners[bannerAtual];
+}
+
+var time = setInterval(trocabanner, 4000);
+var controle = document.querySelector ('.pause');
+
+controle. onclick = function () {
+    if (controle.className == 'pause')
+    {clearInterval(time);
+     controle.className = 'play'   
+    } else {
+        time = setInterval(trocabanner, 4000);
+        controle.className = 'pause' ;
+    } 
+
+    return false;  
+};
